4391d773e788fd83484b86f8ef09eaaaf3699a44
GumSurf <gabriel.christe@gmail.com>
2023-07-04 15:37:29 +0200

    Add : Footerpage

--- portfolio-gumsurf/src/components/Footer.js
+++ portfolio-gumsurf/src/components/Footer.js
@@ -1,12 +1,33 @@
 import LOGO from '../assets/LOGO.png'
+import Github from '../assets/Github.png'
+import Linkedin from '../assets/Linkedin.png'
+import { Link } from 'react-router-dom';
 
 function Footer() {
     return (
         <div className='portfolio-footer'>
-            <img src={LOGO} alt="Logo page web" />
-            <h2 className='kasa-copyright'>© 2023 Gabriel CHRISTE. All rights reserved</h2>
-            <a href='#'>GitHub</a>
-            <a href='#'>Linkedin</a>
+            <div className='div-logo'>
+                <a className='lien-img' href='#'><img className="header-logo" src={LOGO} alt="Logo" /></a>
+            </div>
+            <ul className='footer-links'>
+                <li className='li-links'>
+                    <Link to='https://github.com/GumSurf'><img src={Github} alt="Logo Github"/></Link>
+                </li>
+                <li className='li-links'>
+                    <Link to='https://www.linkedin.com/in/gabriel-christe-000/'><img src={Linkedin} alt="Logo Linkedin"/></Link>
+                </li>
+            </ul>
+            <ul className='footer-contact'>
+                <li>
+                    <p>Christe Gabriel</p>
+                </li>
+                <li>
+                    <p>(+33)6 38 87 05 62</p>
+                </li>
+                <li>
+                    <p>gabriel.christe@gmail.com</p>
+                </li>
+            </ul>
         </div>
     )
 }
