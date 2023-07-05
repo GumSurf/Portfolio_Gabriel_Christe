c2dfd21c7aadda8cf6697c4949f235f56770596f
GumSurf <gabriel.christe@gmail.com>
2023-07-05 03:51:40 +0200

    Add: form for send e-mail

--- portfolio-gumsurf/src/components/Contact.js
+++ portfolio-gumsurf/src/components/Contact.js
@@ -1,22 +1,51 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import emailjs from 'emailjs-com';
 
 function Contact() {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    const onSubmit = (data) => {
+        const templateParams = {
+            from_name: data.name,
+            from_email: data.email,
+            message: data.message,
+        };
+
+        emailjs.send('service_4stcns8', 'template_nev0679', templateParams, 'sqKMJg9tpN0LBAKLD')
+            .then(() => {
+                console.log('Email sent successfully!');
+                templateParams.reset();
+            })
+            .catch((error) => {
+                console.error('Erreur lors de l\'envoi de l\'e-mail:', error);
+            });
+    };
+
+
     return (
         <div className='contact'>
             <div className='background-color'>
-                <h1 className='contact-me '>CONTACTEZ-MOI</h1>
-                <div className='div-color'>
-                    <h2 className='title-adresse, info-contact'>ADRESSE</h2>
-                    <p className='adresse, info-contact'>10, avenue de la libération 56700 Hennebont</p>
-                </div>
-                <div className='div-color'>
-                    <h2 className='title-telephone, info-contact'>TÉLÉPHONE</h2>
-                    <p className='telephone, info-contact'>(+33)6 38 87 05 62</p>
-                </div>
-                <div className='div-color'>
-                    <h2 className='title-email, info-contact'>E-MAIL</h2>
-                    <p className='email, info-contact'>gabriel.christe@gmail.com</p>
-                </div>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className='div-color'>
+                        <label htmlFor="name">Nom</label>
+                        <input type="text" id="name" {...register('name', { required: true })} />
+                        {errors.name && <span>Ce champ est requis</span>}
+                    </div>
+                    <div className='div-color'>
+                        <label htmlFor="email">Email</label>
+                        <input type="email" id="email" {...register('email', { required: true })} />
+                        {errors.email && <span>Ce champ est requis</span>}
+                    </div>
+                    <div className='div-color'>
+                        <label htmlFor="message">Message</label>
+                        <textarea id="message" {...register('message', { required: true })} />
+                        {errors.message && <span>Ce champ est requis</span>}
+                    </div>
+                    <div className='div-color'>
+                        <button className='button-submit' type="submit">Envoyer</button>
+                    </div>
+                </form>
             </div>
         </div>
     )
