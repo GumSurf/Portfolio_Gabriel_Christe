a1e8d9685e6e70d6b6cf6621c95d389639eb5a75
GumSurf <gabriel.christe@gmail.com>
2023-08-24 15:04:38 +0200

    Add: modifications for media querry tablet and phone

--- portfolio-gumsurf/src/components/Planetes.js
+++ portfolio-gumsurf/src/components/Planetes.js
@@ -1,59 +1,79 @@
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import React, { useState } from 'react';
 import Bleu_Planete from '../assets/bleu_planete.webp';
 import Violette_Planete from '../assets/violette_planete.webp';
 import Rose_Planete from '../assets/rose_planete.webp';
 import Satellite from '../assets/satellite.webp';
 
 function Planetes() {
-    const [isHoveredRose, setIsHoveredRose] = useState(false);
-    const [isHoveredViolette, setIsHoveredViolette] = useState(false);
-    const [isHoveredBleu, setIsHoveredBleu] = useState(false);
-    const [isHoveredSatellite, setIsHoveredSatellite] = useState(false);
+  const [isHoveredRose, setIsHoveredRose] = useState(false);
+  const [isHoveredViolette, setIsHoveredViolette] = useState(false);
+  const [isHoveredBleu, setIsHoveredBleu] = useState(false);
+  const [isHoveredSatellite, setIsHoveredSatellite] = useState(false);
 
-    const handleMouseEnterRose = () => {setIsHoveredRose(true);};
-    const handleMouseLeaveRose = () => {setIsHoveredRose(false);};
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 600) {
+        setIsHoveredRose(true);
+        setIsHoveredViolette(true);
+        setIsHoveredBleu(true);
+        setIsHoveredSatellite(true);
+      }
+    };
 
-    const handleMouseEnterViolette = () => {setIsHoveredViolette(true);};
-    const handleMouseLeaveViolette = () => {setIsHoveredViolette(false);};
+    handleResize();
 
-    const handleMouseEnterBleu= () => {setIsHoveredBleu(true);};
-    const handleMouseLeaveBleu = () => {setIsHoveredBleu(false);};
+    window.addEventListener('resize', handleResize);
 
-    const handleMouseEnterSatellite = () => {setIsHoveredSatellite(true);};
-    const handleMouseLeaveSatellite = () => {setIsHoveredSatellite(false);};
-    return (
-        <div className='div_planetes'>
-            <div className='planete'>
-                <Link className='lien lien_rose_planete' to='/Portfolio_GumSurf/'>
-                    <img className="img_planete rose_planete rotate-self" src={Rose_Planete} alt="Rose Planete" onMouseEnter={handleMouseEnterRose} onMouseLeave={handleMouseLeaveRose} />
-                    {isHoveredRose && <p className='nom nom_rose_planete'>Accueil</p>}
-                </Link>
-            </div>
-            {isHoveredRose && <div className='overlay'></div>}
-            <div className='planete'>
-                <Link className='lien lien_violette_planete' to='/Portfolio_GumSurf/'>
-                    <img className="img_planete violette_planete rotate-self" src={Violette_Planete} alt="Violette Planete" onMouseEnter={handleMouseEnterViolette} onMouseLeave={handleMouseLeaveViolette} />
-                    {isHoveredViolette && <p className='nom nom_violette_planete'>Portfolio</p>}
-                </Link>
-            </div>
-            {isHoveredViolette && <div className='overlay'></div>}
-            <div className='planete'>
-                <Link className='lien lien_bleu_planete' to='/Portfolio_GumSurf/'>
-                    <img className="img_planete bleu_planete rotate-self" src={Bleu_Planete} alt="Bleu Planete" onMouseEnter={handleMouseEnterBleu} onMouseLeave={handleMouseLeaveBleu} />
-                    {isHoveredBleu && <p className='nom nom_bleu_planete'>A propos de moi</p>}        
-                </Link>
-            </div>
-            {isHoveredBleu && <div className='overlay'></div>}
-            <div className='planete'>
-                <Link className='lien lien_satellite' to='/Portfolio_GumSurf/'>
-                    <img className="img_planete satellite rotate-self" src={Satellite} alt="Satellite" onMouseEnter={handleMouseEnterSatellite} onMouseLeave={handleMouseLeaveSatellite} />
-                    {isHoveredSatellite && <p className='nom nom_satellite'>Contact</p>}        
-                </Link>
-            </div>
-            {isHoveredSatellite && <div className='overlay'></div>}
-        </div>
-    );
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const handleMouseEnterRose = () => { setIsHoveredRose(true); };
+  const handleMouseLeaveRose = () => { setIsHoveredRose(false); };
+
+  const handleMouseEnterViolette = () => { setIsHoveredViolette(true); };
+  const handleMouseLeaveViolette = () => { setIsHoveredViolette(false); };
+
+  const handleMouseEnterBleu = () => { setIsHoveredBleu(true); };
+  const handleMouseLeaveBleu = () => { setIsHoveredBleu(false); };
+
+  const handleMouseEnterSatellite = () => { setIsHoveredSatellite(true); };
+  const handleMouseLeaveSatellite = () => { setIsHoveredSatellite(false); };
+
+  return (
+    <div className='div_planetes'>
+      <div className='planete'>
+        <Link className='lien lien_rose_planete' to='/Portfolio_GumSurf/'>
+          <img className="img_planete rose_planete rotate-self" src={Rose_Planete} alt="Rose Planete" onMouseEnter={handleMouseEnterRose} onMouseLeave={handleMouseLeaveRose} />
+          {isHoveredRose ? ( <p className='nom nom_rose_planete'>Accueil</p> ) : null}
+        </Link>
+      </div>
+      {isHoveredRose && <div className='overlay'></div>}
+      <div className='planete'>
+        <Link className='lien lien_violette_planete' to='/Portfolio_GumSurf/'>
+          <img className="img_planete violette_planete rotate-self" src={Violette_Planete} alt="Violette Planete" onMouseEnter={handleMouseEnterViolette} onMouseLeave={handleMouseLeaveViolette} />
+          {isHoveredViolette ? ( <p className='nom nom_violette_planete'>Portfolio</p> ) : null}
+        </Link>
+      </div>
+      {isHoveredViolette && <div className='overlay'></div>}
+      <div className='planete'>
+        <Link className='lien lien_bleu_planete' to='/Portfolio_GumSurf/'>
+          <img className="img_planete bleu_planete rotate-self" src={Bleu_Planete} alt="Bleu Planete" onMouseEnter={handleMouseEnterBleu} onMouseLeave={handleMouseLeaveBleu} />
+          {isHoveredBleu ? ( <p className='nom nom_bleu_planete'>A propos de moi</p> ) : null}
+        </Link>
+      </div>
+      {isHoveredBleu && <div className='overlay'></div>}
+      <div className='planete'>
+        <Link className='lien lien_satellite' to='/Portfolio_GumSurf/'>
+          <img className="img_planete satellite rotate-self" src={Satellite} alt="Satellite" onMouseEnter={handleMouseEnterSatellite} onMouseLeave={handleMouseLeaveSatellite} />
+          {isHoveredSatellite ? ( <p className='nom nom_satellite'>Contact</p> ) : null}
+        </Link>
+      </div>
+      {isHoveredSatellite && <div className='overlay'></div>}
+    </div>
+  );
 }
 
 export default Planetes;
