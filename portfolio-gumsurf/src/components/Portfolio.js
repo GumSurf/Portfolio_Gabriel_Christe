424c962ee8455b8699580589008016a2bedfe5ab
GumSurf <gabriel.christe@gmail.com>
2024-02-05 20:43:14 +0100

    Add: Link for 'Sophie Bluel' and 'Mon Vieux Grimoire'

--- portfolio-gumsurf/src/components/Portfolio.js
+++ portfolio-gumsurf/src/components/Portfolio.js
@@ -115,15 +115,20 @@ function Body() {
                     </div>
                 </div>
                 <div className='SophieBluel, projet'>
+                <Link to="https://gumsurf.github.io/Portfolio-architecte-sophie-bluel/">
                     <div className="image-container">
                         <img src={SophieBluel} alt="site web Sophie Bluel" />
                         <div className='overlay'>
-                            <h2 className='projet-title'>Sophie Bluel</h2>
+                        <Link to="https://gumsurf.github.io/Portfolio-architecte-sophie-bluel/"><h2 className='projet-title'>Sophie Bluel</h2></Link>
                             <div className='div-projet-filtre'>
                                 <p className='projet-filtre'>Site Html / Css || Site web portfolio d'une architecte d'intérieur</p>
+                                <div className='div-cliquez-ici'>
+                                    <p className='cliquez-ici'>Cliquez ici !</p>
+                                </div>
                             </div>
                         </div>
                     </div>
+                </Link>
                     <div className='div-competences-problemes'>
                         <div className='div-competences'>
                             <h2 className='title-competences'>Compétences apprises</h2>
@@ -161,15 +166,20 @@ function Body() {
                     </div>
                 </div>
                 <div className='Booki, projet'>
+                <Link to="https://monvieuxgrimoire-gc-6f14ede2407f.herokuapp.com/">
                         <div className="image-container">
                             <img src={MonVieuxGrimoire} alt="site web Mon Vieux Grimoire" />
                             <div className='overlay'>
-                                <h2 className='projet-title'>Mon Vieux Grimoire</h2>
+                            <Link to="https://monvieuxgrimoire-gc-6f14ede2407f.herokuapp.com/"><h2 className='projet-title'>Mon Vieux Grimoire</h2></Link>
                                 <div className='div-projet-filtre'>
                                     <p className='projet-filtre'>Site Backend, Node.js, Express.js, MongoDb || Site web de notation de livres</p>
+                                    <div className='div-cliquez-ici'>
+                                        <p className='cliquez-ici'>Cliquez ici !</p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
+                </Link>
                     <div className='div-competences-problemes'>
                         <div className='div-competences'>
                             <h2 className='title-competences'>Compétences apprises</h2>
